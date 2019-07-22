8d2d2348789af2895e63411cfba87223e6524a62
12bng <12bng2@gmail.com>
2019-07-22 13:26:52 +0200

    Inicio con los jsp

---
+++ WebContent/WEB-INF/vistas/includes/cabecera.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Ejemplo JavaEE</title>
+<base href="${pageContext.request.contextPath}/" />
+<link rel="stylesheet" href="css/estilos.css" />
+</head>
+<body>
+	<header>
+		<h1>Ejemplo JavaEE</h1>
+	</header>
---
+++ src/com/ipartek/formacion/uf2218/controlador/LogoutServlet.java
@@ -0,0 +1,24 @@
+package com.ipartek.formacion.uf2218.controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+	private static final String LOGIN = "/WEB-INF/vistas/login.jsp";
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().invalidate();
+		request.getRequestDispatcher(LOGIN).forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
